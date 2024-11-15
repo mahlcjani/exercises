f03338d95b5fd6d6cbc97257ab8e2da532ce34d1
Maciej Misiołek <mahlcjani@proton.me>
2024-11-15 08:44:25 +0100

    A bit nicer condition

--- src/step1/arrays.js
+++ src/step1/arrays.js
@@ -1,7 +1,7 @@
 
 export function reverseArray(array) {
     if (array !== undefined) {
-        for (let head = 0, tail = array.length-1; head < array.length/2; head++, tail--) {
+        for (let head = 0, tail = array.length-1; head < tail; head++, tail--) {
             let x = array[head];
             array[head] = array[tail];
             array[tail] = x;
